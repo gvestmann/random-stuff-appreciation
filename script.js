7059b955b8477835ae9a791c8bc4acf0bec73037
gvestmann <gvestmann@icloud.com>
2020-10-15 21:04:18 +0200

    first commit // html + css ready

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+const cardContent = [
+    {
+    tite: "Albatross",
+    text: "Badass motherfucker riding the storm",
+    image: "img/albatross.jpg",
+    category: "nature",
+    love: false,
+    },
+    {
+    tite: "Stoned dogs",
+    text: "So human, I categorized them as people",
+    image: "img/dogs.jpg",
+    category: "people",
+    love: false,
+    },
+    {
+    tite: "Dolores",
+    text: "Do you ever question the nature of your reality?",
+    image: "img/dolores.jpg",
+    category: "people",
+    love: false,
+    },
+    {
+    tite: "Uncle Bobby",
+    text: "Hey there, mister nobel price winning tambourine man",
+    image: "img/dylan.jpg",
+    category: "",
+    love: false,
+    },
+]
